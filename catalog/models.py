2281db64949cc51564520bc057d279ab17195346
vitalinho <kirillovitaly@mail.ru>
2024-01-02 15:09:05 +0300

    20.1 step 2

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+
+
+    class Meta():
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+
+class Category(models.Model):
+
+
+    class Meta():
+        verbose_name = 'каталог'
+        verbose_name_plural = 'каталоги'
