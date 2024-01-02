f118fd5a016991a9fe44daaf39a3972571d31535
vitalinho <kirillovitaly@mail.ru>
2024-01-02 14:14:01 +0300

    20.1 step 1

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Django_Skypro',
+        'USER': 'postgres',
+        'PASSWORD': '1234'
     }
 }
 
