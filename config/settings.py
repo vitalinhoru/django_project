5d73d48a59b62fb53b12ad19814e2290147b70e0
vitalinho <kirillovitaly@mail.ru>
2024-01-31 19:00:15 +0300

    22.2 - Step 2

--- config/settings.py
+++ config/settings.py
@@ -139,3 +139,13 @@ MEDIA_ROOT = BASE_DIR / 'media'
 AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'vitalinhoru@yandex.ru'
+EMAIL_HOST_PASSWORD = 'uelvwahzajrcebaj'
+EMAIL_USE_SSL = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
 
@@ -10,6 +12,16 @@ class RegisterView(CreateView):
     form_class = UserRegisterForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        new_user = form.save()
+        send_mail(
+            subject='Поздравляем с регистрацией',
+            message='Вы зарегистрировались на нашей платформе!',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[new_user.email]
+        )
+        return super().form_valid(form)
 
 
 class ProfileView(UpdateView):
