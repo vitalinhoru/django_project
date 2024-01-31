b5597ccec180d96b5cbfc21209e8616f3c703470
vitalinho <kirillovitaly@mail.ru>
2024-01-31 19:29:19 +0300

    22.2 - Step 2

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
+import random
+
 from django.conf import settings
 from django.core.mail import send_mail
-from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView
 
 from users.forms import UserRegisterForm, UserProfileForm
@@ -31,3 +34,17 @@ class ProfileView(UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+
+def generate_new_password(request):
+    new_password = ''.join([str(random.randint(0, 9)) for _ in range(12)])
+    send_mail(
+        subject='Восстановление пароля',
+        message=f'Ваш новый пароль: {new_password}',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[request.user.email]
+    )
+    request.user.set_password(new_password)
+    request.user.save()
+
+    return redirect(reverse('catalog:index'))
